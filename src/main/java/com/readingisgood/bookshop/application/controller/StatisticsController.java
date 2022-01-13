b16fb89a875e6e85e0904e5c39abd61dc819941b
GkcAp <gapayding@gmail.com>
2022-01-14 00:20:53 +0300

    application.properties changes

--- src/main/java/com/readingisgood/bookshop/application/controller/StatisticsController.java
+++ src/main/java/com/readingisgood/bookshop/application/controller/StatisticsController.java
@@ -3,14 +3,22 @@ package com.readingisgood.bookshop.application.controller;
 import com.readingisgood.bookshop.application.service.statistics.StatisticsService;
 import com.readingisgood.bookshop.application.service.statistics.request.GetStatisticsCustomersMonthlyRequest;
 import com.readingisgood.bookshop.application.service.statistics.response.GetStatisticsCustomersMonthlyResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 
 import static org.springframework.http.HttpStatus.OK;
 
+@Slf4j
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/statistics")
 public class StatisticsController {
 
     StatisticsService statisticsService;
